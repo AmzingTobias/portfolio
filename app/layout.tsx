16581fd992fc4eb0a7647df9d28023f0a86e3872
Tobias <tobiasdunn.02@gmail.com>
2025-06-23 08:03:55 +0100

    Change gap size in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
         className={`${geistMono.className} bg-background flex flex-col antialiased max-w-screen`}
       >
         <Navbar />
-        <div className="bg-accent flex flex-col pt-14 gap-14">
+        <div className="bg-accent flex flex-col pt-10 gap-6">
           <main className="flex flex-col gap-[32px] items-center sm:items-start">
-            <div className="flex flex-col w-full items-center justify-items-center min-h-screen gap-16">
+            <div className="flex flex-col w-full items-center justify-items-center min-h-screen gap-10">
               <div className="w-full max-w-md sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-screen-2xl flex flex-col gap-10">
                 {children}
               </div>
